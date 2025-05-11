7699f73b993e1919ca0b52b6f884f2aa3d1e1211
SohaibKhan007 <sohaibkhanatwork@gmail.com>
2025-05-11 18:44:40 +0530

    done with the kafka setup now it is working properly

---
+++ analytics-service/src/main/java/com/pm/analyticsservice/AnalticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.analyticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalticsServiceApplication.class, args);
+	}
+
+}
---
+++ analytics-service/src/test/java/com/pm/analtics_service/AnalticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.analtics_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
