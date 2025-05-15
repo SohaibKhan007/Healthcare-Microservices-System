241728f267e220eea966fff05a811c10dad71cbc
SohaibKhan007 <sohaibkhanatwork@gmail.com>
2025-05-15 18:58:11 +0530

    done with services

---
+++ intigration-tests/src/main/java/com/pm/intigrationtest/IntigrationtestApplication.java
@@ -0,0 +1,13 @@
+package com.pm.intigrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntigrationtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntigrationtestApplication.class, args);
+	}
+
+}
